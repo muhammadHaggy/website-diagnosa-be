89265e8f142b470ba9b9adfa5865eb237d54e729
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-09-23 14:12:40 +0700

    setup https

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python manage.py migrate --noinput  # Run migrations
-gunicorn diagnosa_backend.wsgi:application --bind 0.0.0.0:8000  # Start Gunicorn
+exec gunicorn diagnosa_backend.wsgi:application --bind 0.0.0.0:8000  # Start Gunicorn
