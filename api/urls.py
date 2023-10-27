49d3dac2117114258d1ac92ed25c53f44f0149ac
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-10-27 19:18:23 +0700

    add register basic user and login user, admin

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('',views.getData),
     path('pred/',views.getPred),
     path('add/', views.addData)
