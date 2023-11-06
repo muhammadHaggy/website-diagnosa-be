00b2aec41d975f214f77df35b248f37a8beca09a
Muhammad Haggy <muhammad.haggy@ui.ac.id>
2023-11-06 13:28:11 +0700

    add first_name, last_name, and age

--- base/models.py
+++ base/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
 
 class FormData(models.Model):
     is_pulmonary_TB = models.BooleanField(default=False)
@@ -18,3 +17,12 @@ class IPAPrediction(models.Model):
     is_high_risk = models.BooleanField(default=False)
     submission_date = models.DateTimeField(default=timezone.now)
     submitted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="predictions", default=None)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
